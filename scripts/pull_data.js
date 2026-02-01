de8f5f1d3ae67a542a9cb5bf4b491158f6a9f960
sore0711 <sore0711@umn.edu>
2026-02-01 17:58:29 -0600

    force refresh of pages

--- scripts/pull_data.js
+++ scripts/pull_data.js
@@ -1,4 +1,4 @@
-fetch("indie-animation-newsletter/articles.JSON")
+fetch("Indie-Animation-Newsletter/articles.JSON")
   .then(response => response.json())
   .then(data => {
     // 'data' is now an array of articles
